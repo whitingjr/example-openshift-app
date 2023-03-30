d2a9f9129b77f25c7606c7c75ac9099da015a85f
Jeremy Whiting <jwhiting@redhat.com>
2023-03-30 18:39:35 +0100

    Load other properties.sh file to set NAMESPACE. Ordered to set APP_NAME relevant to context.

--- postgres/install.sh
+++ postgres/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+. ../eapxp4/properties.sh
 . properties.sh
 
-oc new-app postgresql-ephemeral \
+oc -n ${NAMESPACE} new-app postgresql-ephemeral \
    -p DATABASE_SERVICE_NAME=${APP_NAME} \
    -p POSTGRESQL_DATABASE=frdemo \
    -p POSTGRESQL_PASSWORD=frdemo \
